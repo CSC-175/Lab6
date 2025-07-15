0dc8d5b36c94ca3fe8a4d9762143319010bb7e90
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-07-15 13:28:54 -0400

    Initial Commit

--- Lab6.cpp
+++ Lab6.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    // place your code for the Find Sum of All Digits of a Number program here
+    // Place your code for the Find Sum of All Digits of a Number program here
     
     return 0;
 }
