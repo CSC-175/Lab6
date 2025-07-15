f793a848b105019d931a488cf1d548675855b410
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-07-15 12:00:14 -0400

    Initial Commit

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,14 +1,8 @@
-// Write a program that prompts the user to enter a multiple 
-// digit positive integer then use a loop to iteratively 
-// sum each of the digits and print the sum to the screen. 
-// For example, if the number 2451 was entered the sum of those digits is 12.
-
 #include <iostream>
 using namespace std;
 
-// place your code for the Find Sum of All Digits of a Number program here
-
 int main() {
+    // place your code for the Find Sum of All Digits of a Number program here
     
     return 0;
 }
